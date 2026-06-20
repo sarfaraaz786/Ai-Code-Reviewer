513b7ac5657edfee11ba13fb4a29d893a0c90dbc
Sarfaraaz <mdsarfaraaz86@gmail.com>
2026-06-20 18:52:49 +0530

    Fix production cookies

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -43,7 +43,12 @@ async function resgisterUserController(req,res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie('token',token);
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None',
+        maxAge: 24 * 60 * 60 * 1000
+    });
 
     res.status(201).json({
         message: "User Registered Successfully",
@@ -86,7 +91,12 @@ async function loginUserController(req,res) {
         {expiresIn: '1d'}
     )
 
-    res.cookie('token',token);
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None',
+        maxAge: 24 * 60 * 60 * 1000
+    });
 
     res.status(201).json({
         message: "User LoggedIn Successfully",
@@ -112,7 +122,11 @@ async function logoutUserController(req, res) {
         await tokenBlacklistModel.create({ token });
     }
 
-    res.clearCookie('token')
+    res.clearCookie('token', {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None'
+    });
 
     res.status(200).json({
         message: "User logged out successfully"
